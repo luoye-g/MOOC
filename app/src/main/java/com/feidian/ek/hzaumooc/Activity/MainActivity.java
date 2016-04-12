5d6bd5bf9060b9426e92f5468ca86ce5eb96c2a8
M.G <624586383@qq.com>
2016-04-12 18:27:41 +0800

    分享界面的改善  黄宇

--- app/src/main/java/com/feidian/ek/hzaumooc/Activity/MainActivity.java
+++ app/src/main/java/com/feidian/ek/hzaumooc/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.feidian.ek.hzaumooc.Activity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,18 +10,28 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.GridView;
+import android.widget.PopupWindow;
 
 import com.feidian.ek.hzaumooc.Bean.MainViewTitle;
 import com.feidian.ek.hzaumooc.R;
+import com.feidian.ek.hzaumooc.View.Adapter.ShareAdapter;
 import com.feidian.ek.hzaumooc.View.ListDivider;
 import com.feidian.ek.hzaumooc.View.Adapter.MainAdapter;
+import com.feidian.ek.hzaumooc.download.DowloadAcivity;
+import com.feidian.ek.hzaumooc.share.ShareActivty;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    PopupWindow popupWindow;
 
     @Bind(R.id.main_list) RecyclerView recyclerView;
 
@@ -67,7 +78,7 @@ public class MainActivity extends BaseActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_gallery) {
-            Intent intent =new Intent(this,VideoActivity.class);
+            Intent intent =new Intent(this,DowloadAcivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
@@ -77,7 +88,9 @@ public class MainActivity extends BaseActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            showPopuptWindow();
+            /*Intent intent =new Intent(this,ShareActivty.class);
+            startActivity(intent);*/
         } else if (id == R.id.nav_send) {
 
         }
@@ -85,5 +98,49 @@ public class MainActivity extends BaseActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    void showPopuptWindow()
+    {
+        LayoutInflater layoutInflater=getLayoutInflater();
+        View view =layoutInflater.inflate(R.layout.share_gridview,null);
+        GridView gridview=(GridView)view.findViewById(R.id.share_grid);
+        gridview.setAdapter(new ShareAdapter(this));
+        popupWindow=new PopupWindow(view, WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
+
+        popupWindow.setFocusable(true);
+        popupWindow.setTouchable(true);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                ClosePop();
+            }
+        });
+
+
+
+        // 实例化一个ColorDrawable颜色为半透明
+        //ColorDrawable dw = new ColorDrawable(0xb0000000);
+        ColorDrawable dw=new ColorDrawable(0xffffffff);
+        popupWindow.setBackgroundDrawable(dw);
+
+
+        // 设置popWindow的显示和消失动画
+        popupWindow.setAnimationStyle(R.style.mypopwindow_anim_style);
+        // 在底部显示
+        popupWindow.showAtLocation( recyclerView,
+                Gravity.BOTTOM, 0, 0);
+        WindowManager.LayoutParams layoutParams=this.getWindow().getAttributes();
+        layoutParams.alpha=0.5f;
+        this.getWindow().setAttributes(layoutParams);
+    }
+    void ClosePop()
+    {
+        if(popupWindow!=null ){
+        popupWindow.dismiss();
+        popupWindow=null;
+        }
+        WindowManager.LayoutParams params=this.getWindow().getAttributes();
+        params.alpha=1.0f;
+        this.getWindow().setAttributes(params);
     }
 }
